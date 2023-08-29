cf54cd72f81ca3365a3039bba5dcb46f53fe574c
gem-RishabhZutshi <88335034+gem-RishabhZutshi@users.noreply.github.com>
2023-08-29 17:45:03 +0530

    Update deploy_azure.sh

--- scripts/deploy_azure.sh
+++ scripts/deploy_azure.sh
@@ -24,3 +24,6 @@ docker push $ECR_URL/$NAME:latest
 # deploy to aks cluster
 az aks get-credentials --resource-group Test --name airflowlocaltest
 kubectl set image deployment/airflow-webserver airflow-webserver=$ECR_URL/$NAME:latest
+kubectl set image deployment/airflow-scheduler airflow-scheduler=$ECR_URL/$NAME:latest
+kubectl set image deployment/airflow-worker airflow-worker=$ECR_URL/$NAME:latest
+kubectl set image deployment/airflow-flower airflow-flower=$ECR_URL/$NAME:latest
