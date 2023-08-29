5a148b8d3621e40a22a71cb22ec2b067ec9f09eb
gem-RishabhZutshi <88335034+gem-RishabhZutshi@users.noreply.github.com>
2023-08-29 17:04:10 +0530

    Create deploy_azure.sh

---
+++ scripts/deploy_azure.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ENV=$1
+echo "Deploying webservice to $ENV"
+
+NAME=airflowlocaltest-$ENV
+ACR_NAME=airflowlocaltest$ENV
+echo "Deploying webservice to Azure for $NAME"
+REGION=us-east
+ECR_URL="$ACR_NAME.azurecr.io"
+
+COMMIT_HASH=`date +%Y%m%d%H%M%S`
+echo "COMMIT_HASH: $COMMIT_HASH"
+
+echo "Building image: $NAME:latest"
+docker build --rm -t $NAME:latest .
+
+az acr login --name $ACR_NAME
+
+# tag and push image using latest
+docker tag $NAME $ECR_URL/$NAME:latest
+docker push $ECR_URL/$NAME:latest
