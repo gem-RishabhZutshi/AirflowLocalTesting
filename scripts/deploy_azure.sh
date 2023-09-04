f0f3afdee312beec36568909e02016d0f938e40a
gem-RishabhZutshi <88335034+gem-RishabhZutshi@users.noreply.github.com>
2023-09-04 18:16:13 +0530

    Update deploy_azure.sh

--- scripts/deploy_azure.sh
+++ scripts/deploy_azure.sh
@@ -12,14 +12,14 @@ ECR_URL="$ACR_NAME.azurecr.io"
 COMMIT_HASH=`date +%Y%m%d%H%M%S`
 echo "COMMIT_HASH: $COMMIT_HASH"
 
-echo "Building image: $NAME:V2"
-docker build --rm -t $NAME:V2 .
+echo "Building image: $NAME:latest"
+docker build --rm -t $NAME:latest .
 
 az acr login --name $ACR_NAME
 
 #tag and push image using latest
-docker tag $NAME $ECR_URL/$NAME:V2
-docker push $ECR_URL/$NAME:V2
+docker tag $NAME $ECR_URL/$NAME:latest  
+docker push $ECR_URL/$NAME:latest
 
 #deploy to aks cluster
 az aks get-credentials --resource-group Test --name airflowlocaltest
@@ -31,12 +31,12 @@ kubectl config current-context
 echo "View cluster information:"
 kubectl cluster-info
 
-# kubectl set image deployment/airflow-webserver airflow-webserver=$ECR_URL/$NAME:V2
-# kubectl set image deployment/airflow-scheduler airflow-scheduler=$ECR_URL/$NAME:V2
-# kubectl set image deployment/airflow-worker airflow-worker=$ECR_URL/$NAME:V2
-# kubectl set image deployment/airflow-flower airflow-flower=$ECR_URL/$NAME:V2
+kubectl set image deployment/airflow-webserver airflow-webserver=$ECR_URL/$NAME:latest
+kubectl set image deployment/airflow-scheduler airflow-scheduler=$ECR_URL/$NAME:latest
+kubectl set image deployment/airflow-worker airflow-worker=$ECR_URL/$NAME:latest
+kubectl set image deployment/airflow-flower airflow-flower=$ECR_URL/$NAME:latest
 
-# kubectl rollout status deployment airflow-webserver
-# kubectl rollout status deployment airflow-scheduler
-# kubectl rollout status deployment airflow-worker
-# kubectl rollout status deployment airflow-flower
+kubectl rollout status deployment airflow-webserver
+kubectl rollout status deployment airflow-scheduler
+kubectl rollout status deployment airflow-worker
+kubectl rollout status deployment airflow-flower
