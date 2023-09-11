e8e955e1d5311f1e2bed132add28a6f51b7d961a
gem-RishabhZutshi <88335034+gem-RishabhZutshi@users.noreply.github.com>
2023-09-11 16:10:51 +0530

    Create deploy_aws.sh

---
+++ scripts/deploy_aws.sh
@@ -0,0 +1,19 @@
+NAME=$1
+AWS_ACCOUNT=${2:-791167451670}
+REGION=us-east-1
+ECR_URL="$AWS_ACCOUNT.dkr.ecr.$REGION.amazonaws.com"
+CLUSTER="arn:aws:ecs:$REGION:$AWS_ACCOUNT:cluster/$NAME"
+
+COMMIT_HASH=`date +%Y%m%d%H%M%S`
+echo $COMMIT_HASH
+
+cp config/airflow_$AWS_ACCOUNT.cfg config/airflow.cfg
+
+echo "Building image: $NAME:latest"
+docker build --rm -t $NAME:latest .
+
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_URL
+
+# tag and push image using latest
+docker tag $NAME $ECR_URL/$NAME:latest
+docker push $ECR_URL/$NAME:latest
