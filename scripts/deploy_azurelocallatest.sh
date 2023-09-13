504b6f4e827b7c62c3bf7662a1f84820c95f9c19
gem-RishabhZutshi <88335034+gem-RishabhZutshi@users.noreply.github.com>
2023-09-13 17:25:23 +0530

    Create deploy_azurelocallatest.sh

---
+++ scripts/deploy_azurelocallatest.sh
@@ -0,0 +1,86 @@
+FROM python:3.7-slim-buster
+LABEL maintainer="Puckel_"
+
+# Never prompt the user for choices on installation/configuration of packages
+ENV DEBIAN_FRONTEND noninteractive
+ENV TERM linux
+
+# Airflow
+ARG AIRFLOW_VERSION=1.10.9
+ARG AIRFLOW_USER_HOME=/usr/local/airflow
+ARG AIRFLOW_DEPS=""
+ARG PYTHON_DEPS=""
+ENV AIRFLOW_HOME=${AIRFLOW_USER_HOME}
+
+# Define en_US.
+ENV LANGUAGE en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV LC_ALL en_US.UTF-8
+ENV LC_CTYPE en_US.UTF-8
+ENV LC_MESSAGES en_US.UTF-8
+
+RUN set -ex \
+    && buildDeps=' \
+        freetds-dev \
+        libkrb5-dev \
+        libsasl2-dev \
+        libssl-dev \
+        libffi-dev \
+        libpq-dev \
+        redis-tools \
+        git \
+    ' \
+    && apt-get update -yqq \
+    && apt-get upgrade -yqq \
+    && apt-get install -yqq --no-install-recommends \
+        $buildDeps \
+        freetds-bin \
+        build-essential \
+        default-libmysqlclient-dev \
+        apt-utils \
+        curl \
+        rsync \
+        netcat \
+        redis-tools \
+        locales \
+    && sed -i 's/^# en_US.UTF-8 UTF-8$/en_US.UTF-8 UTF-8/g' /etc/locale.gen \
+    && locale-gen \
+    && update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 \
+    && useradd -ms /bin/bash -d ${AIRFLOW_USER_HOME} airflow \
+    && pip install -U pip setuptools wheel \
+    && pip install pytz \
+    && pip install pyOpenSSL \
+    && pip install ndg-httpsclient \
+    && pip install pyasn1 \
+    && pip install apache-airflow[crypto,celery,postgres,hive,jdbc,mysql,ssh${AIRFLOW_DEPS:+,}${AIRFLOW_DEPS}]==${AIRFLOW_VERSION} \
+    && pip install markupsafe==2.0.1 \
+    && pip install 'redis==3.2' \
+    && pip install --upgrade WTForms \
+    && if [ -n "${PYTHON_DEPS}" ]; then pip install ${PYTHON_DEPS}; fi 
+    
+
+RUN pip install --upgrade pip && \
+pip install --upgrade awscli 
+
+
+COPY config/entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+COPY config/airflow.cfg ${AIRFLOW_HOME}/airflow.cfg
+COPY dags ${AIRFLOW_HOME}/dags
+COPY config/webserver_config.py ${AIRFLOW_HOME}/webserver_config.py
+
+RUN chown -R airflow: ${AIRFLOW_HOME}
+
+ENV PYTHONPATH ${AIRFLOW_HOME}
+
+USER airflow
+
+COPY requirements.txt .
+RUN pip install --user --no-cache-dir -r requirements.txt
+RUN mkdir ${AIRFLOW_HOME}/tmp
+
+
+EXPOSE 8080 5555 8793
+
+WORKDIR ${AIRFLOW_HOME}
+ENTRYPOINT ["/entrypoint.sh"]
