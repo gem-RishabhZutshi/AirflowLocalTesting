e82e8c5186caddd507fe0cec9502e212e40407a4
gem-RishabhZutshi <88335034+gem-RishabhZutshi@users.noreply.github.com>
2023-12-11 15:42:30 +0530

    Create all_dags.py

---
+++ dags/all_dags.py
@@ -0,0 +1,21 @@
+import os
+from airflow.models import Variable
+
+env = os.getenv("AIRFLOW_ENVS") or Variable.get("AIRFLOW_ENVS", default_var="dev")
+os.environ["env"] = env
+os.environ["CLIENT_ID"] = os.getenv("CLIENT_ID") or Variable.get("CLIENT_ID")
+os.environ["CLIENT_SECRET"] = os.getenv("CLIENT_SECRET") or Variable.get("CLIENT_SECRET")
+os.environ["TENANT_ID"] = os.getenv("TENANT_ID") or Variable.get("TENANT_ID")
+print(f"Current env: {env}")
+print(f"CLIENT_ID: {CLIENT_ID}")
+print(f"CLIENT_SECRET: {CLIENT_SECRET}")
+print(f"TENANT_ID: {TENANT_ID}")
+
+if env != "local":
+    os.unsetenv("CLIENT_ID")
+    os.unsetenv("CLIENT_SECRET")
+    os.unsetenv("TENANT_ID")
+
+print(f"CLIENT_ID: {CLIENT_ID}")
+print(f"CLIENT_SECRET: {CLIENT_SECRET}")
+print(f"TENANT_ID: {TENANT_ID}")
